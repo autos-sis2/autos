e8f6b3f2bfefc1046fb95f961cc8dd03855bedf4
omarpepito <omarmamani408@gmail.com>
2016-11-07 19:26:02 -0700

    es el catalogo de los autos si conexion a base de datos

---
+++ proyectoSisDos/src/catalogo/frame_principal.java
@@ -0,0 +1,83 @@
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+
+public class frame_principal extends JFrame {
+    JLabel label,titulo,label1;
+    JPanel panel_botones,principal,prueba=null;
+    JMenuBar barra;
+    JMenu inicio , ingresar;
+    JMenuItem ingresar1 ,catalogo;
+            
+    Test mipane;
+  
+    public frame_principal(){
+        setLayout(new BorderLayout());
+        Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize(); 
+         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+         setBounds(0,0,1200, 800);
+
+        setVisible(true);
+        
+    
+        panel_botones=new JPanel();
+        panel_botones.setLayout(new BorderLayout());
+        
+        titulo = new JLabel("CATALOGO DE VEHICULOS");
+        titulo.setForeground(Color.red);
+        titulo.setFont(new Font("Arial", 3, 40));
+        
+        label = new JLabel();
+        label1 = new JLabel();
+       
+        barra = new JMenuBar();
+        
+        inicio = new JMenu("INICIO");
+        catalogo = new JMenuItem("catalogo");
+        inicio.add(catalogo);
+        barra.add(inicio);
+        
+        ingresar = new JMenu("INGRESAR");
+        ingresar1 = new JMenuItem("ingresar1");
+        ingresar.add(ingresar1);
+        barra.add(ingresar);
+        
+        
+        panel_botones.add(titulo,BorderLayout.CENTER);
+        panel_botones.add(barra,BorderLayout.SOUTH);
+
+        principal=new JPanel();
+        principal.setLayout(new BorderLayout(50,30));
+        add(panel_botones,BorderLayout.NORTH);
+
+        mipane = new Test();
+        add(mipane,BorderLayout.CENTER);
+        pack();
+        
+        ingresar1.addActionListener(new ActionListener() {
+
+           
+            public void actionPerformed(ActionEvent ae) {
+                setVisible(false);
+                frame_sesion mipane = new frame_sesion();
+            }
+
+        });
+    }
+
+}
