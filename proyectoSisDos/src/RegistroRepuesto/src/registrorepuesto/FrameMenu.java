5c275fea47da993cb0ee9377bbbe605bb2039b4a
daniela <danielabravorueda@gmail.com>
2016-11-08 15:41:25 -0800

    se hizo la interfaz del repuestos de; auto

---
+++ proyectoSisDos/src/RegistroRepuesto/src/registrorepuesto/FrameMenu.java
@@ -0,0 +1,23 @@
+
+package registrorepuesto;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Daniela
+ */
+public class FrameMenu extends JFrame {
+    
+  
+  public FrameMenu()
+  {
+      
+      MenuPanel milamina = new MenuPanel();
+        add(milamina);
+        setResizable(false);
+        pack();
+  
+  }
+    
+}
---
+++ proyectoSisDos/src/RegistroRepuesto/src/registrorepuesto/Main.java
@@ -0,0 +1,31 @@
+
+package registrorepuesto;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Daniela
+ */
+public class Main  {
+
+   
+    public static void main(String[] args) {
+        
+     FrameMenu miframe  = new FrameMenu();
+     miframe.setVisible(true);
+        //miframe.setResizable(false);
+        miframe.setBounds(500, 0, 780, 750);
+        miframe.setLocationRelativeTo(null);
+        miframe.setDefaultCloseOperation(miframe.EXIT_ON_CLOSE);
+    
+
+    }
+}      
+       
+    
+    
+
+    
